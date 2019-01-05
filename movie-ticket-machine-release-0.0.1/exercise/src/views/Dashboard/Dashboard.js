67e3ac6c4beea0e3f8421a570f1fcba388a00089
nirattikan <5831302008@lamduan.mfu.ac.th>
2019-01-05 14:26:00 +0700

    component showmovie

--- movie-ticket-machine-release-0.0.1/exercise/src/views/Dashboard/Dashboard.js
+++ movie-ticket-machine-release-0.0.1/exercise/src/views/Dashboard/Dashboard.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import Movie from '../component/showmovie'
+import { connect } from 'react-redux'
+
+import { getdetailMovie }  from '../actions/detailmovie'
+
 
 class Dashboard extends Component {
+
+  componentDidMount() {
+    this.props._onLoadDataMovie()
+  
+  }   
+
   render() {
+    console.log("testdata",(this.props.datamovie)? this.props.datamovie : null)
     return (
       <div className="animated fadeIn">
-        Hello world!
-      </div>
+       <Movie details = {(this.props.datamovie) ? this.props.datamovie: null}/>
+    </div>
     );
   }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => ({
+  datamovie : state.detailmovie.detail
+})
+
+const mapDispatchToProps = dispatch => ({
+  _onLoadDataMovie: () => dispatch(getdetailMovie())
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Dashboard)
